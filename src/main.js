71c1f0810607dc3df3002cb358ea7c5d19676212
Juan Carlos Orrego <jcorrego@gmail.com>
2021-06-27 21:24:46 -0500

    Add element plus library

--- src/main.js
+++ src/main.js
@@ -4,10 +4,9 @@ import GAuth from 'vue3-google-oauth2'
 import './index.css'
 import router from "./router"
 import store from './store'
+import installElementPlus from './plugins/element'
 const gAuthOptions = { clientId: '578655982087-b0878kv0a4hlqcubg590nkm2u1n8f2u9.apps.googleusercontent.com'}
 
-createApp(App)
-    .use(router)
-    .use(store)
-    .use(GAuth, gAuthOptions)
-    .mount('#app')
+const app = createApp(App)
+installElementPlus(app)
+app.use(router).use(store).use(GAuth, gAuthOptions).mount('#app')
