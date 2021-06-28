096956c1de51777167b7168a3145f6c763a0a811
Juan Carlos Orrego <jcorrego@gmail.com>
2021-06-28 02:25:13 -0500

    Display table

---
+++ src/components/tables/TaskList.vue
@@ -0,0 +1,126 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <div class="flex flex-col mt-5">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+          <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+              <tr>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Date
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Hours
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Category
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Comments
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  OT
+                </th>
+              </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+              <tr v-for="(task,index) in tasks" :key="index">
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="flex items-center">
+                    <div class="ml-4">
+                      <div class="text-sm font-medium text-gray-900">
+                        {{ task.date }}
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-sm text-gray-900">{{ task.hours }}</div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-sm text-gray-900">{{ task.category }}</div>
+                  <div class="text-sm text-gray-500">{{ task.description }}</div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-sm text-gray-900">{{ task.comments }}</div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <span v-if="task.overtime" class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
+                    Yes
+                  </span>
+                  <span v-else class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                    No
+                </span>
+                </td>
+
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const tasks = [
+  {
+    date: '2021-06-28',
+    hours: '01:30',
+    category: 'Development',
+    description: 'Code Review',
+    comments: 'Ticket #234, solve the problem',
+    overtime: false,
+  },
+  {
+    date: '2021-06-28',
+    hours: '01:30',
+    category: 'Development',
+    description: 'Code Review',
+    comments: 'Ticket #234, solve the problem',
+    overtime: false,
+  },
+  {
+    date: '2021-06-28',
+    hours: '01:30',
+    category: 'Development',
+    description: 'Code Review',
+    comments: 'Ticket #234, solve the problem',
+    overtime: true,
+  },
+    {
+    date: '2021-06-28',
+    hours: '01:30',
+    category: 'Development',
+    description: 'Code Review',
+    comments: 'Ticket #234, solve the problem',
+    overtime: false,
+  },
+  {
+    date: '2021-06-28',
+    hours: '01:30',
+    category: 'Development',
+    description: 'Code Review',
+    comments: 'Ticket #234, solve the problem',
+    overtime: false,
+  },
+  {
+    date: '2021-06-28',
+    hours: '01:30',
+    category: 'Development',
+    description: 'Code Review',
+    comments: 'Ticket #234, solve the problem',
+    overtime: true,
+  },
+
+]
+
+export default {
+  setup() {
+    return {
+      tasks,
+    }
+  },
+}
+</script>
--- src/components/views/TrackTime.vue
+++ src/components/views/TrackTime.vue
@@ -11,8 +11,9 @@
           >Section title</h2>
           <new-time-record></new-time-record>
           <div class="mb-5 rounded-lg bg-white overflow-hidden shadow">
-            <div class="p-6 h-64 text-center">
+            <div class="p-6 text-center">
               <time-period-filter></time-period-filter>
+              <task-list></task-list>
             </div>
           </div>
         </section>
@@ -20,7 +21,24 @@
 
       <!-- Right column -->
       <div class="grid grid-cols-1 gap-4">
+
         <HourStats v-if="!selectedProject" />
+
+        <section
+          v-if="!selectedProject"
+          aria-labelledby="section-2-title"
+        >
+          <div class="rounded-lg overflow-hidden shadow">
+            <div class="bg-white">
+              <div class="px-4 py-5 border-b border-gray-200">
+                <h3 class="text-lg leading-6 font-medium text-gray-900">
+                  For consolidated view we can add here some details about missing / overtime hours for each project...
+                </h3>
+              </div>
+            </div>
+          </div>
+        </section>
+
         <section
           v-if="selectedProject"
           aria-labelledby="section-2-title"
@@ -45,9 +63,9 @@
             </div>
             <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
               <router-link
-                  to="/project/settings"
-                  class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
-                >Go to project configuration</router-link>
+                to="/project/settings"
+                class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+              >Go to project configuration</router-link>
             </div>
           </div>
         </section>
@@ -112,6 +130,7 @@ import HourStats from '../stats/HourStats.vue'
 import NewTimeRecord from '../forms/NewTimeRecord.vue'
 import AvatarWithName from '../users/AvatarWithName.vue'
 import TimePeriodFilter from '../filters/TimePeriodFilter.vue'
+import TaskList from '../tables/TaskList.vue'
 
 export default {
     components: {
@@ -119,6 +138,7 @@ export default {
         NewTimeRecord,
         AvatarWithName,
         TimePeriodFilter,
+        TaskList,
     },
     computed: {
         ...mapState({
