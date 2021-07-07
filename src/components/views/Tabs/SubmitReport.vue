63505e42d54c637286523e6d2fd9bfa2ebc47a01
Juan Carlos Orrego <jcorrego@gmail.com>
2021-07-07 07:42:20 -0500

    Preview button

--- src/components/views/Tabs/SubmitReport.vue
+++ src/components/views/Tabs/SubmitReport.vue
@@ -10,8 +10,8 @@
             gap-y-6 gap-x-4
             overflow-hidden
         "
-    >
-        ok
+    >   You can submit your monthly report, please make sure to review all the information before submitting.
+        <submit-button @click="preview">Preview</submit-button>
     </div>
     <div class="px-4 py-3 bg-gray-50 text-right sm:px-6 rounded-b-lg">
         <submit-button @click="submit">Submit report</submit-button>
@@ -24,6 +24,11 @@ import SubmitButton from '../../forms/SubmitButton.vue'
 export default {
     components: {
         SubmitButton,
+    },
+    data(){
+        return {
+
+        }
     },
     methods: {
         submit(){
